e1eaff8e180603682761c46aa927fd6c99cba79e
Sankha Mukherjee <sanhasubhro.mukherjee@cognizant.com>
2018-10-20 09:30:55 -0500

    Ignored local props

--- src/main/java/com/motashishu/barkeep/BarkeepApplication.java
+++ src/main/java/com/motashishu/barkeep/BarkeepApplication.java
@@ -2,9 +2,10 @@ package com.motashishu.barkeep;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BarkeepApplication {
+public class BarkeepApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BarkeepApplication.class, args);
